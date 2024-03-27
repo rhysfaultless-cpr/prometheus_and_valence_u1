be9251a804091b72df247e6ea21ca1fc427b34f2
Rhys Faultless <rfaultless@clearpathrobotics.com>
2024-03-27 11:53:42 -0400

    Increased tab spacing for methods

--- prometeus_and_valence_u1/prometeus_and_valence_u1/prometeus_and_valence_u1.py
+++ prometeus_and_valence_u1/prometeus_and_valence_u1/prometeus_and_valence_u1.py
@@ -52,18 +52,17 @@ class PrometheusAndValenceU1(Node):
         self.create_subscription(BatteryState, '/bmu_2/battery_state', self.battery2_state_callback, 10)
         self.create_subscription(BatteryState, '/bmu_3/battery_state', self.battery3_state_callback, 10)
 
+    def battery1_state_callback(self, msg):
+        self.battery_percentages[1] = msg.percentage
+        prometheus_gauge_1.set(msg.percentage)
 
-def battery1_state_callback(self, msg):
-    self.battery_percentages[1] = msg.percentage
-    prometheus_gauge_1.set(msg.percentage)
+    def battery2_state_callback(self, msg):
+        self.battery_percentages[2] = msg.percentage
+        prometheus_gauge_2.set(msg.percentage)
 
-def battery2_state_callback(self, msg):
-    self.battery_percentages[2] = msg.percentage
-    prometheus_gauge_2.set(msg.percentage)
-
-def battery3_state_callback(self, msg):
-    self.battery_percentages[3] = msg.percentage
-    prometheus_gauge_3.set(msg.percentage)
+    def battery3_state_callback(self, msg):
+        self.battery_percentages[3] = msg.percentage
+        prometheus_gauge_3.set(msg.percentage)
 
 def main(args=None):
     start_http_server(9100)
